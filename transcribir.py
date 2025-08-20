0afff3673a9c9b8254981c8195dd796b4a78d970
JMacalupu <manuelestelo23@gmail.com>
2025-08-19 20:43:03 -0500

    python formateado

--- transcribir.py
+++ transcribir.py
@@ -1,7 +1,18 @@
 import whisper
 import sys
+import io
 
-audio_file = sys.argv[1]  # el archivo que Node le pasa
+# Forzar salida UTF-8 en cualquier consola
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+# Archivo de audio que recibe como argumento
+audio_file = sys.argv[1]
+
+# Cargar modelo Whisper
 model = whisper.load_model("base")
+
+# Transcribir
 result = model.transcribe(audio_file)
+
+# Imprimir texto con tildes y ñ correctas
 print(result["text"])
